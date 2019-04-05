bd27f62198547095598cbd15d7e3d088f0b0b641
RicardodeOliveira <Ricardofbdeoliveira@live.com>
2019-04-04 22:54:31 -0300

    CRUD

--- gestao_produtos/settings.py
+++ gestao_produtos/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'gestao_produtos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['my_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=30)
+    product_description = models.CharField(max_length=30)
+    product_amount = models.IntegerField()
+    product_value = models.DecimalField(max_digits=19, decimal_places=10)
+
+    def __str__(self):
+        return self.product_name
